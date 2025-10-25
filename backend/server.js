298e90cb754e597344a8b47602c56cd5f549427c
mohammad126529 <mohammadaburob2@gmail.com>
2025-10-25 15:20:55 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';    
+import authRoutes from './routes/auth.route.js';
+import connectDB from '../backend/db/connecToMongoDB.js';
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000; 
+
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => { 
+    res.send('Hello World!'); 
+});
+
+console.log(connectDB());
+app.listen(PORT, () => {
+    
+    
+    console.log(`Server is running at http://localhost:${PORT}`);
+    
+});
